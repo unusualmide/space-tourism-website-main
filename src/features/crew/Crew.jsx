340ab9108dcdc7316dd24e9b0a036175b6beef26
unusualmide <ajilogbaayomide34@gmail.com>
2023-12-28 14:51:16 +0100

    commit me

--- src/features/crew/Crew.jsx
+++ src/features/crew/Crew.jsx
@@ -5,25 +5,25 @@ import PageNav from "../../components/PageNav"
 const crewDetails = [
   {
     "name": "Douglas Hurley",
-      "images": "/public/crew/image-douglas-hurley.png",
+      "images": "/crew/image-douglas-hurley.png",
       "role": "Commander",
       "bio": "Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2."
   },
   {
     "name": "Mark Shuttleworth",
-    "images": "/public/crew/image-mark-shuttleworth.png",
+    "images": "/crew/image-mark-shuttleworth.png",
     "role": "Mission Specialist",
     "bio": "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist."
   },
   {
     "name": "Victor Glover",
-    "images": "/public/crew/image-victor-glover.png",
+    "images": "/crew/image-victor-glover.png",
     "role": "Pilot",
     "bio": "Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer."
   },
   {
     "name": "Anousheh Ansari",
-    "images": "/public/crew/image-anousheh-ansari.png",
+    "images": "/crew/image-anousheh-ansari.png",
     "role": "Flight Engineer",
     "bio": "Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space."
   }
--- src/features/destination/Destination.jsx
+++ src/features/destination/Destination.jsx
@@ -9,7 +9,7 @@ const details = [
     travelTime: "3 days",
     description:
       "See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
-    img: "/public/destination/image-moon.png",
+    img: "/destination/image-moon.png",
   },
   {
     name: "Mars",
@@ -17,7 +17,7 @@ const details = [
     travelTime: "9 months",
     description:
       "Don’t just wonder at the red planet from afar; take a trip to the Olympus Mons, the largest volcano in the solar system, in style. You’ll marvel at the Valles Marineris, a canyon system that makes the Grand Canyon look like a crack in the ground.",
-    img: "/public/destination/image-mars.png",
+    img: "/destination/image-mars.png",
   },
   {
     name: "Europa",
@@ -25,7 +25,7 @@ const details = [
     travelTime: "3 years",
     description:
       "If you’re looking for a truly out of this world experience, you’ve found it. Europa is the smallest of the four Galilean moons orbiting Jupiter, and the sixth-closest to the planet. It’s also the sixth-largest moon in the Solar System. Europa’s surface is smooth, with few surface features, such as craters and mountains, instead having a cracked, icy crust.",
-    img: "/public/destination/image-europa.png",
+    img: "/destination/image-europa.png",
   },
   {
     name: "Titan",
@@ -33,7 +33,7 @@ const details = [
     travelTime: "7 years",
     description:
       "Titan is the largest moon of Saturn and the second-largest natural satellite in the Solar System. It is the only moon known to have a dense atmosphere, and the only known body in space, other than Earth, where clear evidence of stable bodies of surface liquid has been found.",
-    img: "/public/destination/image-titan.png",
+    img: "/destination/image-titan.png",
   },
 ];
 
